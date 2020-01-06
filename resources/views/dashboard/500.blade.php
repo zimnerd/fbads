c43756e6cc15de02876d2e5f6336ba710761dd54
zimnerd <eddienyagano@gmail.com>
2020-01-06 12:14:10 +0200

    add new changes

--- resources/views/dashboard/500.blade.php
+++ resources/views/dashboard/500.blade.php
@@ -7,7 +7,7 @@
         <div class="col-md-6">
           <div class="clearfix">
             <h1 class="float-left display-3 mr-4">500</h1>
-            <h4 class="pt-3">Houston, we have a problem!</h4>
+            <h4 class="pt-3">Oooh, we have a problem!</h4>
             <p class="text-muted">The page you are looking for is temporarily unavailable.</p>
           </div>
           <div class="input-prepend input-group">
